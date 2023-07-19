0562314b43c8dec2285051d5ea52e447b1c98d54
-Taha Dostifam‍- <mr.tahadostifam@gmail.com>
2023-07-20 00:57:40 +0330

    Refactor cart validations

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -15,66 +15,62 @@ class CartController < ApplicationController
   ##
   # The `add_item` function adds a product to the user's cart and updates the cart's total.
   def add_item
-    validate_params!(AddItemToCartSchema, params) {
-        @session = Session.find_by(user_id: current_user.id)
+    @session = Session.find_by(user_id: current_user.id)
 
-        unless @session
-          @session = Session.create!(user_id: current_user.id)
-        end
+    unless @session
+      @session = Session.create!(user_id: current_user.id)
+    end
 
-        @item = CartItem.find_by(product_id: params[:product_id])
+    @item = CartItem.find_by(product_id: params[:product_id])
 
-        if @item
-          @item.quantity += params[:quantity]
-        else
-          @item = CartItem.create!(product_id: params[:product_id], session_id: @session.id, quantity: params[:quantity])
-        end
+    if @item
+      @item.quantity += params[:quantity]
+    else
+      @item = CartItem.create!(product_id: params[:product_id], session_id: @session.id, quantity: params[:quantity])
+    end
 
-        if @item.save
-          @session.compute_total
-          @session.save
+    if @item.save
+      @session.compute_total
+      @session.save
 
-          render json: { msg: "Item added" }, status: :ok
-        else
-          render json: { msg: "Unable to add item", detail: { errors: @item.errors } }, status: :ok
-        end
-      }
+      render json: { msg: "Item added" }, status: :ok
+    else
+      render json: { msg: "Unable to add item", detail: { errors: @item.errors } }, status: :bad_request
+    end
   end
 
   ##
   # The `remove_item` function removes a cart item from the database and returns a JSON response indicating whether the
   # item was successfully removed or not.
   def remove_item
-      validate_params!(RemoveCartItem, params) {
-        @item = CartItem.find_by(id: params[:item_id])
+    @item = CartItem.find_by(id: params[:item_id])
 
-        if @item
-          @item.destroy
+    if @item
+      @item.destroy
 
-          render json: { msg: "Item removed" }, status: :ok
-        else
-          render json: { msg: "Item not found" }, status: :bad_request
-        end
-      }
+      render json: { msg: "Item removed" }, status: :ok
+    else
+      render json: { msg: "Item not found" }, status: :bad_request
+    end
   end
 
   ##
   # The `update_item` function updates the quantity of a cart item and computes the total for the session it belongs to.
   def update_item
-    validate_params!(UpdateCartItem, params) {
-        @item = CartItem.find_by(id: params[:item_id])
+    @item = CartItem.find_by(id: params[:item_id])
 
-        if @item
-          @item.update(quantity: params[:quantity])
+    if @item
+      if @item.update(quantity: params[:quantity])
+        @item.session.compute_total
+        @item.session.save
 
-          @item.session.compute_total
-          @item.session.save
-
-          render json: { msg: "Item updated" }, status: :ok
-        else
-          render json: { msg: "Item not found" }, status: :bad_request
-        end
-      }
+        render json: { msg: "Item updated" }, status: :ok
+      else
+        render json: { msg: "Unable to add item", detail: { errors: @item.errors } }, status: :bad_request
+      end
+    else
+      render json: { msg: "Item not found" }, status: :bad_request
+    end
   end
 
   private
